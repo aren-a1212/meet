921ab297add995dfb42cd98c46e6eb5ea2c80647
aren-a1212 <abdelazizahmed1212@gmail.com>
2025-05-21 08:14:21 -0500

    new tests

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import NumberOfEvents from "../components/NumberOfEvents";
+
+describe("<NumberOfEvents /> Component", ()=>{
+    let NumberOfEventsComponent;
+    beforeEach(()=>{
+        NumberOfEventsComponent = render (
+            <NumberOfEvents
+				currentNOE={32}
+				setCurrentNOE={() => {}}
+				setErrorAlert={() => {}}
+			/>
+        );
+    });
+
+    test("component contains input textbox", ()=>{
+        const input= NumberOfEventsComponent.queryByRole("textbox");
+        expect (input).toBeInTheDocument();
+    });
+    test('ensures default value of textboxis 32', ()=>{
+        const input= NumberOfEventsComponent.queryByRole("textbox");
+        expect (input).toHaveValue("32");
+    });
+    test("textbox value changes when user updates input", async () => {
+        const input= NumberOfEventsComponent.getByTestId("numberOfEventsInput");
+        const user = userEvent.setup();
+		await user.clear(input);
+		await user.type(input, "10");
+		expect(input).toHaveValue("10");
+});
+});
+
+   
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
  
-const Event = ()=>{
-    return(
-    <li></li>
-    );
-};
+const Event = ({event})=>{
+const [showDetails, setShowDetails]= useState(false);    
+   
+        return (
+            <li className="event">
+              <h2>{event.summary}</h2>
+              <p>{event.location}</p>
+              <p>{new Date(event.created).toUTCString()}</p>
+        
+              <button
+                className="details-btn"
+                onClick={() => setShowDetails(!showDetails)}
+              >
+                {showDetails ? "hide details" : "show details"}
+              </button>
+        
+              {showDetails && (
+                <div className="details">
+                  <p>{event.description}</p>
+                  <a href={event.htmlLink} target="_blank" rel="noopener noreferrer">
+                    see details
+                  </a>
+                </div>
+              )}
+            </li>
+          );
+        };
+        
 export default Event;
