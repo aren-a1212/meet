a0f9e71da84775c50cb9acf600a77ba036db2fb6
aren-a1212 <abdelazizahmed1212@gmail.com>
2025-06-07 10:50:03 -0500

    adding and Remove atatus

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import * as atatus from 'atatus-spa';
+atatus.config('cbf7d642b886450eb9f6b7857a9458f1').install();
+
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
