5fc01484a7ed4c479a0f5079e726bdfdaa77cad7
aren-a1212 <abdelazizahmed1212@gmail.com>
2025-06-11 09:49:42 -0500

    added icons and service-worker

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-import * as atatus from 'atatus-spa';
-atatus.config('cbf7d642b886450eb9f6b7857a9458f1').install();
+//import * as atatus from 'atatus-spa';
+//atatus.config('cbf7d642b886450eb9f6b7857a9458f1').install();
 
 
 
@@ -13,4 +14,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+serviceWorkerRegistration.register();
