5f2cf2bcf1b6e0f4e59821860fd84313b3f69800
aren-a1212 <abdelazizahmed1212@gmail.com>
2025-06-14 14:48:18 -0500

    changes

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -37,7 +37,7 @@ const CityEventsChart = ({ allLocations, events }) => {
         <CartesianGrid />
         <XAxis
           type="category" dataKey="city" name="City"
-          angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
+          angle={-45} interval={0} textAnchor="end"  tick={{ dx: 20, dy: 40, fontSize: 12 }}
         />
         <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false}  />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
